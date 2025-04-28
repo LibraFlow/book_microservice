7d5c87e593e25002100d5aae49d28455bb5267ba
i459452 <459452@student.fontys.nl>
2025-04-28 12:42:51 +0300

    Test revision

--- book_service/app/src/main/java/backend2/App.java
+++ book_service/app/src/main/java/backend2/App.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class App {
     @GetMapping("/")
     public String getGreeting() {
-        return "Hello World! Revision 3!";
+        return "Hello World! Revision 4!";
     }
 
     public static void main(String[] args) {
